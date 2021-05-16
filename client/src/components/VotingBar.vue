b728dcb35e7f307ad8ae688c889496c09845e476
Daria Anikanova <daria.i.anikanova@gmail.com>
2021-05-16 14:21:51 -0400

    WIP Voting Update

--- client/src/components/VotingBar.vue
+++ client/src/components/VotingBar.vue
@@ -1,14 +1,30 @@
 <template>
-  <v-card>
-    <v-card-title>Time to Vote! Who is the real AI?</v-card-title>
+  <div>
+    <h2 class="mb-2">Time to Vote! Who is the real AI?</h2>
     <div class="grid-container">
-      <v-btn
+      <!-- Change Card Colour based on player -->
+      <v-card
+        class="pa-2"
+        color="grey"
         v-for="(player, index) in room.rounds[room.round].playerNames"
         :key="index"
-        @click="$store.dispatch('vote', player)"
-      >{{player}}</v-btn>
+      >
+        <div class="d-flex justify-space-between align-center">
+          <!-- Put Avatars Here -->
+          <v-avatar size="56">
+            <img
+              alt="Player Avatar"
+              src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+            />
+          </v-avatar>
+          {{player}}
+          <v-card-action>
+            <v-btn color="success" @click="$store.dispatch('vote', player)">Vote!</v-btn>
+          </v-card-action>
+        </div>
+      </v-card>
     </div>
-  </v-card>
+  </div>
 </template>
 
 <script>
@@ -21,7 +37,12 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.vote-container {
+  padding: 1rem;
+}
 .grid-container {
   display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-gap: 0.5rem;
 }
 </style>
